1007d8b57b79222c84e51f240ca4e51c3ea089ec
YaroslavBukhgolts <55090332+YaroslavBukhgolts@users.noreply.github.com>
2022-11-01 15:22:37 +0300

    Homework 16

--- test.js
+++ test.js
@@ -1,6 +1,39 @@
-"use strict";
+'use strict';
 
-// для вывода в консоль vs code используем комманду node test.js
-console.log("Hello, Synergy!");
-console.log(5 + 2);
-console.log(70 / 2);
+// 1
+/*
+    JSON - общий формат для представления значений и объектов. Предназначен для взаимодействия между фронтом и бэкендом, другими словами между клиентом и сервером.
+*/
+
+// 2 
+// {'name':'John','age': 18,'isAdmin':true}; - объект формата JSON 
+let obj = {
+    name: 'Alex',
+    age: 25,
+    isStudent: true,
+}
+let newJson = JSON.stringify(obj);
+console.log(newJson); // {"name":"Alex","age":25,"isStudent":true}
+console.log(typeof newJson); // string
+
+let obj1 = JSON.parse(newJson); // преобразуем JSON в объект
+console.log(obj1);
+console.log(typeof obj1); // object
+
+// 3
+/*
+    JSON поддерживает следующие типы даннных:
+    1. Объекты
+    2. Массивы
+    3. Примитивы:
+        - строки
+        - числа
+        - булевы значения
+        - null
+*/
+console.log(JSON.stringify(1)); // 1
+console.log(JSON.stringify('apple')); // "apple"
+console.log(JSON.stringify(true)); // true
+console.log(JSON.stringify(null)); // null
+console.log(JSON.stringify([1, 2, 3, 4])); // [1, 2, 3, 4]
+console.log(JSON.stringify({name:'Alex', age: 25})); // {"name":"Alex","age":25}
