551532f7ffdb2dcd269471109d352c3b06d8b531
YaroslavBukhgolts <55090332+YaroslavBukhgolts@users.noreply.github.com>
2022-11-02 14:18:57 +0300

    Homework 17

--- test.js
+++ test.js
@@ -1,6 +1,27 @@
-"use strict";
+'use strict';
 
-// для вывода в консоль vs code используем комманду node test.js
-console.log("Hello, Synergy!");
-console.log(5 + 2);
-console.log(70 / 2);
+// 1
+/*
+    рекурсия - это вызов функции самой себя
+*/
+
+// 2
+// итеративный способ
+function pow1(x, n){
+    let res = 1;
+    for(let i = 0; i < n; i++){
+        res *= x;
+    }
+    return res;
+}
+
+let val1 = pow1(2, 4);
+console.log(val1);
+
+// рекурсивынй способ
+function recurtionPow(x, n){
+    return (n !==1)? x * recurtionPow(x, n - 1) : x;
+}
+
+let val2 = recurtionPow(2, 3);
+console.log(val2);
