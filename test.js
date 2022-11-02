a38a55d8c8f45bdb44958ae6ca23545eb754c6eb
YaroslavBukhgolts <55090332+YaroslavBukhgolts@users.noreply.github.com>
2022-11-02 19:53:17 +0300

    Add files via upload

--- test.js
+++ test.js
@@ -1,6 +1,22 @@
-"use strict";
+'use strict';
 
-// для вывода в консоль vs code используем комманду node test.js
-console.log("Hello, Synergy!");
-console.log(5 + 2);
-console.log(70 / 2);
+// 1 
+/*
+    метод get предназначен для чтения, метод set для записи.
+*/
+
+// 2
+let obj = {
+    model: 'Gibson',
+    price: 1500,
+    get pricePosition(){
+        console.log(`${this.model} - ${this.price}`); // Gibson - 1500
+    },
+    set pricePosition(value){
+        [this.model, this.price] = value.split(' ');
+    },
+}
+
+obj.pricePosition; // Gibson - 1500
+obj.pricePosition = "Fender 2000";
+obj.pricePosition; // Fender - 2000
