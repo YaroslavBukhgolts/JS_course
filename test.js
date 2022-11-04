f1cddc84ef843159a9a128af576ce626433d10c7
YaroslavBukhgolts <55090332+YaroslavBukhgolts@users.noreply.github.com>
2022-11-04 17:05:12 +0300

    Homework 21

--- test.js
+++ test.js
@@ -1,6 +1,24 @@
-"use strict";
+'use strict';
 
-// для вывода в консоль vs code используем комманду node test.js
-console.log("Hello, Synergy!");
-console.log(5 + 2);
-console.log(70 / 2);
+// 1
+/*
+    Асинхронный код - это код, в котором во время выполнения одного участка кода, была возможность использовать другой код.
+*/
+
+// 2
+/*
+    Callback-функция, функция, которая передается как аргумент в другой функции, и выполняется после того как другая функция завершит свое выполнение. Callback-функции используются в асинхронном методе программирования.  
+*/
+
+// 3 
+function loadScript(src, callback) {
+
+    let script = document.createElement('script');
+  
+    script.lsrc = src;
+  
+    script.onload = () => callback(script);
+  
+    document.head.append(script);
+  
+  }
