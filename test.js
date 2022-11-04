da7646a28c3db1dadb2b11de7a07048f1c96c93d
YaroslavBukhgolts <55090332+YaroslavBukhgolts@users.noreply.github.com>
2022-11-04 15:26:37 +0300

    Homework 20

--- test.js
+++ test.js
@@ -1,6 +1,39 @@
-"use strict";
+'use strict';
 
-// для вывода в консоль vs code используем комманду node test.js
-console.log("Hello, Synergy!");
-console.log(5 + 2);
-console.log(70 / 2);
+// 1
+/*
+    Класс - шаблон для создания объектов с какими-либо общими признаками, позволяет установить начальные значения (свойства) и реализацию поведения (методы)
+*/
+
+// 2 Синтаксис класса
+class MyClass {
+    constructor(title, price){ // конструтор
+        this.title = title,
+        this.price = price
+    }
+
+    saler = 'Guitar Shop'; // свойство
+    
+    get title(){ // метод геттер
+        return this._title;
+    }
+
+    set title(value){ // метод сеттер
+        this._title = value;
+    }
+    get priceItem(){
+        return `${this.title} - ${this.price}`
+    }
+    method1() {} // метод 1
+    method2() {} // метод 2
+}
+
+let sale1 = new MyClass('BCRich', 3000);
+console.log(sale1);
+
+console.log(sale1.saler);
+
+// 3
+/*
+    class по своему типу является функцией. 
+*/
